80ff705c604ecdc3d561ec80e7266074a3eec3f7
HaeTi <t9h9@naver.com>
2024-02-07 19:32:42 +0900

    [브루트포스] 일곱난쟁이 입력과 동시에 초기화하기 sum 연산자 for 문 범위 신경쓰기 sort 연산자

---
+++ src/main/kotlin/baekjoon/브루트포스/일곱난쟁이.kt
@@ -0,0 +1,22 @@
+package baekjoon.브루트포스
+
+// boj 2309
+
+fun main() {
+    val heightArr: Array<Int> = Array(9) { readln().toInt() }
+    var sum = heightArr.sum()
+
+    for (i in 0 until 8) {
+        for (j in i + 1 until 9) {
+           if (sum - heightArr[i] - heightArr[j] == 100) {
+               heightArr[i] = 0
+               heightArr[j] = 0
+
+               heightArr.sort()
+
+               for (k in 2 until 9) println(heightArr[k])
+               return
+           }
+        }
+    }
+}
