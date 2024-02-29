c5d3796558fa9158b6b24be401d114c58529e00e
HaeTi <t9h9@naver.com>
2024-03-01 02:41:14 +0900

    [백트래킹] N과 M(3)

---
+++ src/main/kotlin/baekjoon/백트래킹/N과 M(3).kt
@@ -0,0 +1,29 @@
+package baekjoon.백트래킹
+
+// boj 15651
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+    val arr = IntArray(m)
+
+    fun dfs(start: Int) {
+        if (start == m) {
+            for (i in 0 until m) bw.write("${arr[i]} ")
+            bw.newLine()
+            return
+        }
+
+        for (i in 1..n) {
+            arr[start] = i
+            dfs(start + 1)
+        }
+    }
+
+    dfs(0)
+    bw.flush()
+    bw.close()
+
+}
