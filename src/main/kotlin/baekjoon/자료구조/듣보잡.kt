8c345d16564c78e3a5074b1379ed319ef1eb701d
HaeTi <t9h9@naver.com>
2024-03-11 22:27:16 +0900

    [자료구조] 듣보잡

---
+++ src/main/kotlin/baekjoon/자료구조/듣보잡.kt
@@ -0,0 +1,31 @@
+package baekjoon.자료구조
+
+// boj 1764
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+    val set = LinkedHashSet<String>(n)
+    val result = mutableListOf<String>()
+
+    repeat(n) {
+        set.add(br.readLine())
+    }
+
+    repeat(m) {
+        val input = br.readLine()
+        if (set.contains(input)) result.add(input)
+    }
+
+    bw.write(result.size)
+    for (i in result) {
+        bw.write(i)
+        bw.newLine()
+    }
+
+    bw.flush()
+    bw.close()
+
+}
