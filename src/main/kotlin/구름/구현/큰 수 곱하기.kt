d93bdd692505fda66e1a7b41999837f10a6b55f0
HaeTi <t9h9@naver.com>
2024-05-20 10:55:12 +0900

    [난이도2] 큰 수 곱하기

---
+++ src/main/kotlin/구름/구현/큰 수 곱하기.kt
@@ -0,0 +1,12 @@
+package 구름.구현
+
+fun main(args: Array<String>) {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    val input = br.readLine()
+    bw.write("100000000000000000000")
+    bw.flush()
+    bw.close()
+
+}
