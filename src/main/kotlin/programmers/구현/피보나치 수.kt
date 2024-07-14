2ff440722c6fea3d431d8290cef6b56b5be4ca89
HaeTi <t9h9@naver.com>
2024-07-14 23:28:30 +0900

    [Level2] 피보나치 수

---
+++ src/main/kotlin/programmers/구현/피보나치 수.kt
@@ -0,0 +1,17 @@
+package programmers.구현
+
+class `피보나치 수` {
+    fun solution(n: Int): Int {
+        val arr = IntArray(n+1)
+
+        arr[0] = 0
+        arr[1] = 1
+
+        for (i in 0 until n-1) {
+            arr[i+2] = arr[i+1] + arr[i]
+        }
+
+
+        return arr[n]
+    }
+}
